6f77d2fee40acafd5ee133b3807f2e849f85524a
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2016-02-25 16:32:22 -0500

    beginnings of confidence measurement

--- confidence.py
+++ confidence.py
@@ -2,12 +2,21 @@ import signature
 import numpy as np
 import sys
 
-# Run in same folder as "Stratton_signatures.txt"
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print "Usage: python confidence.py [stratton signature path]"
+        sys.exit(0)
+    else:
+        stratton = signature.load_stratton_signatures(sys.argv[1])	
+	generate_decomposition_bank(stratton['signatures'        
+        
 
-def generate_decomposition_bank():
+def generate_decomposition_bank(stratton_signatures):
+    def random_signature():
+        # First draw random mixture
     raise "Not yet implemented - will randomly generate 10000 signatures "+\
             "and decompose them so that confidence can be quickly queried."
 
-def confidence(num_mutations, decomposition):
+def confidence(num_mutations, decomposition, threshold):
     raise "Not yet implemented - will return a vector of p-values using an "+\
             "existing decomposition bank."
